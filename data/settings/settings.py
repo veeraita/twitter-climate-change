fea5a10dc05b1e1cf2c95f380c4de543b1e454bf
Nuutti Sten <nuutti.sten@aalto.fi>
2020-02-13 21:54:33 +0200

    settings file: the program is launched with a settings file that contains credential and keyword filepaths, output file paths, runtime setup etc. only interaction with the program is for decrypting the twitter credentials.

---
+++ data/settings/settings.py
@@ -0,0 +1,13 @@
+
+class Settings():
+    def __init__(self):
+        """
+        Settings should contain:
+        credentialsfile: encrypted credentials binary file path
+        keywordfile: txt file containing search keywords
+        location: location criterion
+        json_dump: raw json dump file path
+        csv_out: csv output file path
+
+        """
+        
