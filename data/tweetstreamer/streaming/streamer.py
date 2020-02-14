ac56dacda14d1573764317778987ee29febbb0d1
Nuutti Sten <nuutti.sten@aalto.fi>
2020-02-14 12:03:32 +0200

    started working timeout error handling

--- data/tweetstreamer/streaming/streamer.py
+++ data/tweetstreamer/streaming/streamer.py
@@ -1,5 +1,6 @@
 import tweepy
 import sys
+import time
 
 class Streamer(tweepy.StreamListener):
     def __init__(self, json_dump, csv_out):
@@ -8,6 +9,8 @@ class Streamer(tweepy.StreamListener):
         """
         self.json_dump = json_dump
         self.csv_out = csv_out
+        self.reconnection_attempts = 0 #count reconnection attempts
+        self.reconnections_limit = 9
         super(Streamer,self).__init__()
     def on_data(self,data):
         """
@@ -46,6 +49,36 @@ class Streamer(tweepy.StreamListener):
             f.write("%s,%s,%s,%s,%s,%s\n" % (status.created_at,status.user.screen_name,is_retweet,is_quote,text,quoted_text))
 
     def on_error(self, status_code):
+        """
+        twitter recommends immediate reconnection attempt with exponential wait pattern>
+        1. attempt to reconnect
+        2. wait 1,2,4,8, limit seconds
+        3. if limit exceeded, terminate program and notify user
+
+        returning True reconnects the stream
+
+        if disconnection is due to exceeded rate limit, 
+        reset shoulf be waited for or the app might get blacklisted
+
+        http://docs.tweepy.org/en/latest/streaming_how_to.html
+        """
         print("Encountered streaming error (", status_code, ")")
+        print(self.reconnection_attempts," reconnection attempts.")
+        if status_code in [420,429]: # rate limit exceeded
+            # wait until rate limit is reset
+
++
+
+        elif self.reconnection_attempts < self.reconnections_limit:
+            
+            wait_time = 2**self.reconnection_attempts
+            print("Waiting ",wait_time, "s and reconnecting.")
+            time.sleep(wait_time)
+            return True
+        else:
+            print("Reconnection attempts limit exceeded.")
+            print("Terminating.")
+            exit
+
         sys.exit()
 
