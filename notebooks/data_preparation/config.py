7363060fd957a80a4334f9cce986544d37a9d3fc
Veera Itälinna <veera.italinna@aalto.fi>
2020-02-16 14:03:27 +0200

    update instructions for the config file

--- notebooks/data_preparation/config.py
+++ notebooks/data_preparation/config.py
@@ -1,4 +1,8 @@
-# Fill these variables with your own
+"""
+Fill these variables with your own. Don't push to the remote repository!
+To stop git from tracking this file, use the command:
+git update-index --assume-unchanged notebooks/data_preparation/config.py
+"""
 
 consumer_key = ''
 consumer_secret = ''
