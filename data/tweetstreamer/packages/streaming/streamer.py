6005739efa8aca8b91718e10b3224920e2b91b08
Nuutti Sten <nuutti.sten@aalto.fi>
2020-03-03 14:03:09 +0200

    json dump file splitting: new file is created at fixed time intervals. time interval must be decided, but I think daily around 3am could be good - we avoid splitting data in middle of an ongoing conversation as much as possible

--- data/tweetstreamer/packages/streaming/streamer.py
+++ data/tweetstreamer/packages/streaming/streamer.py
@@ -2,19 +2,33 @@ import tweepy
 import sys
 import time
 import logging
+from datetime import datetime, timedelta
+import string
+
 
 class Streamer(tweepy.StreamListener):
     def __init__(self, json_dump, log_file_handler):
         """
         define output files here
         """
+        # create new json dump file every day at certain hour
+        #self.newfiletime = time0.replace(day=x.day, hour=1, minute=0, second=0, microsecond=0) + timedelta(days=1)
+        self.filetime = datetime.now()
+        self.valid_chars = "-_.()%s%s" % (string.ascii_letters, string.digits)
+        
         # Gets or creates a logger
         self.logger = logging.getLogger(__name__) 
         self.logger.setLevel(logging.INFO) 
         # add file handler to logger
         self.logger.addHandler(log_file_handler)
+        self.splittimeperiod = time
         try:
             self.json_dump = json_dump
+            filename = "%s_%s.json"%(self.json_dump,str(self.filetime))
+            self.jsonfilename = ''.join(c for c in filename if c in self.valid_chars)
+            with open(self.jsonfilename, 'w+') as f:
+                #create new file
+                pass
             self.reconnection_attempts = 0 #count reconnection attempts
             self.last_reconnection_time = time.time()
             self.reconnections_limit = 9
@@ -28,8 +42,19 @@ class Streamer(tweepy.StreamListener):
         """
         Dumps everything to a file in json format
         """
+        #reset reconnection attempts
         self.reconnection_attempts = 0
-        with open(self.json_dump, "a") as f:
+        timenow = datetime.now()
+        
+        if timenow >= self.filetime: #time to create new file
+            self.filetime += timedelta(seconds=20)
+            filename = "%s_%s.json"%(self.json_dump,str(self.filetime))
+            self.jsonfilename = ''.join(c for c in filename if c in self.valid_chars) #parse out not allowed characters
+            with open(self.jsonfilename, 'w+') as f:
+                #create new file
+                pass
+
+        with open(self.jsonfilename, "a") as f:
             f.write(data)
             return
     def on_error(self, status_code):
