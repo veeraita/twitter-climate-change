1a558f5b35430ac50d37934987c1fcc053ca4e68
Ville Saarinen <ville.saarinen@tuta.io>
2020-03-31 20:00:14 +0300

    Add pyyaml for more complex config handling

--- data/interactionfollower/packages/settings/settings.py
+++ data/interactionfollower/packages/settings/settings.py
@@ -1,39 +1,58 @@
-import logging
-import csv
+import logging.config
+import yaml
 
 class Settings():
     def __init__(self, stsfile):
         """
-        If k is the number of streams that will be opened, then 
-        settings file should contain k rows in the csv form:
-
-        STREAMING_MODE,CREDENTIALS_PATH,INPUT_USERID_PATH,OUTPUT_RAW_JSON_PATH
+        Reads YAML-setting file.
         """
-        # Gets or creates a logger
-        logging.info("Reading settings file.")
-        try:
-            with open(stsfile, 'r') as set_f:
-                sts_rows = csv.reader(set_f, delimiter=',')
-
-                self.modes, self.filter_cities, self.cred_fps, self.in_fps, self.out_fps = [], [], [], [], []
-                self.n_inst = 0
-                for row in sts_rows:
-                    assert (row[0] == 'follow') or (row[0] == 'track')
-                    self.modes.append(row[0])
-                    assert (row[1] == 'filter') or (row[1] == 'direct')
-                    self.filter_cities.append(row[1]) # Expects "filter" or "direct"
-                    for i in range(2,5):
-                        assert len(row[i]) > 2,"Filenames are not of correct format, need to be longer than 2 characters!" 
-                    self.cred_fps.append(row[2])
-                    self.in_fps.append(row[3])
-                    self.out_fps.append(row[4])
-                    self.n_inst += 1
-                    
-            logging.debug('Path to credentials stored as: {0}'.format(self.cred_fps))
-            logging.debug('Path to input files stored as: {0}'.format(self.in_fps))
-            logging.debug('Path to output files stored as: {0}'.format(self.out_fps))
-        except Exception as ex:
-            logging.error("Error while reading settings: %s",repr(ex))
-            logging.info("Exiting program.")
-            exit()
+        self.logger = logging.getLogger(__name__)
+        self.logger.info("Reading settings file.")
+        self.n_inst = 0
+        ysts = None
+        with open(stsfile, 'r') as set_f:
+            try:
+                ysts = yaml.safe_load(set_f)
+            except yaml.YAMLError as ex:
+                self.logger.error("Error while reading settings: %s",repr(ex))
+                self.logger.info("Exiting program.")
+                exit()
+        
+        self.configs = self._set_variables(ysts) 
     
+    def _set_variables(self, ysts):
+        # parse settings
+        assert isinstance(ysts['update_interval'], int)
+        self.update_interval = ysts['update_interval']
+        assert '.log' == ysts['logfile'][-4:], 'Not a good logfile convention, use .log suffix.'
+        self.logfile = ysts['logfile']
+
+        configs= []
+        for i in ysts['streams']:
+            stream_i = ysts['streams'][i] 
+            len_msg  =  "Filename not of correct format, need to be string" 
+            len_msg += " with length of more than 3 characters." 
+            ssts     = {}                
+            
+            assert (stream_i['mode'] == 'follow') or (stream_i['mode'] == 'track')
+            ssts['mode'] = stream_i['mode']
+            
+            if not isinstance(stream_i['filter'], list):
+                ssts['filter'] = False
+                ssts['filter_output'] = None 
+            else:
+                for w in stream_i['filter']: 
+                    assert isinstance(w, str) and (len(w) > 3)
+                for w in stream_i['filter_output']: 
+                    assert isinstance(w, str) and (len(w) > 3)
+
+                ssts['filter'] = stream_i['filter']
+                ssts['filter_output'] = stream_i['filter_output']
+
+            for w in ['input','output','credentials']:
+                assert len(stream_i[w]) > 3 and isinstance(stream_i[w], str), len_msg
+                ssts[w] = stream_i[w]
+            configs.append(ssts)
+            self.n_inst += 1
+        return configs   
+        
